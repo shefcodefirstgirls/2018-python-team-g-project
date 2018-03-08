c6e5e9af49a100e75466ca94f04df22f30a9a0a2
Kat Koler <katjusa.koler@gmail.com>
2018-03-08 18:50:44 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-python-team-g-project
+# #ShefCodeFirst Python Flask app starting point
+
+A minimal repository containing the core files and folder structure that can be used as a starting
+point for students looking to build and deploy their app for their final course project.
---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask, render_template, request
+
+app = Flask("my_first_app")
+
+@app.route("/")
+def say_hello():
+    return render_template("index.html")
+
+@app.route("/<name>")
+def say_hello_to(name):
+    return render_template("hello.html", user=name)
+
+@app.route("/feedback", methods=["POST"])
+def get_feedback():
+    data = request.values
+
+    return render_template("feedback.html", form_data=data)
+
+"""
+This piece of logic checks whether you are running the app locally or on Heroku
+(make an account at https://www.heroku.com/ before the deployment session!). When
+running the app on Heroku, the PORT environment/config variable is pre-populated by
+Heroku to tell our app the correct port to run on.
+"""
+if "PORT" in os.environ:
+    app.run(host="0.0.0.0", port=int(os.environ["PORT"]))
+else:
+    app.run(debug=True)
