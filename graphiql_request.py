576b327426556e14ed8dfa84dd687b1685898eaa
Darren Vong <d.vongy@gmail.com>
2018-10-31 23:29:49 +0000

    Removed server-side functionalities to show a dumbed down version of the website

--- graphiql_request.py
+++ graphiql_request.py
@@ -2,19 +2,19 @@ import json
 import requests
 import os
 from helpers import fallback_to_file
-# import config #need this locally
+import config #need this locally
 
 
 token = os.environ["github_api_key"]
 
 def get_profiles():
 
-    headers = {'Authorization':'Bearer ' + token}
-    query = '{ repository(owner: "katkoler", name: "cfg_python_project") { collaborators(first: 20) {nodes {name login bio avatarUrl }}}}'
-    r = requests.post('https://api.github.com/graphql', json.dumps({"query": query}), headers=headers)
-    if r.status_code == 200:
-        git_bios = r.json()["data"]["repository"]["collaborators"]["nodes"]
-        return git_bios
-    else:
-        # fallback in case of API key error
-        return fallback_to_file("git_bio.json")
+    # headers = {'Authorization':'Bearer ' + token}
+    # query = '{ repository(owner: "katkoler", name: "cfg_python_project") { collaborators(first: 20) {nodes {name login bio avatarUrl }}}}'
+    # r = requests.post('https://api.github.com/graphql', json.dumps({"query": query}), headers=headers)
+    # if r.status_code == 200:
+    #     git_bios = r.json()["data"]["repository"]["collaborators"]["nodes"]
+    #     return git_bios
+    # else:
+    # fallback in case of API key error
+    return fallback_to_file("git_bio.json")
